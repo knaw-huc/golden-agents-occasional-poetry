ddb8f496286afd4310493199cf756c488fbc8d45
Leon van Wissen <leon@vwissen.nl>
2022-05-18 18:58:14 +0200

    feat: linkset from validated file

--- experiments/results/make_linkset.py
+++ experiments/results/make_linkset.py
@@ -12,7 +12,7 @@ ga = Namespace("https://data.goldenagents.org/datasets/")
 voidPlus = Namespace("https://lenticularlens.org/voidPlus/")
 
 
-def main(infile, outfile):
+def main(infile: str, outfile: str, reification: bool = True):
     """
     Create a (reified) linkset from a GraphEmbeddings result.
 
@@ -21,21 +21,29 @@ def main(infile, outfile):
     for all the combinations of the resources in this cluster. Skolemized
     blank nodes are used as resource identifiers for the rdf:Statement.
 
+    If the file is validated (i.e. there is a column named 'valid'), then
+    the linkset is only created for the valid resources in clusters.
+
     Example:
         ```ttl
-        <https://archief.amsterdam/indexen/deeds/0cda93b1-ad78-4dc1-a835-42b1249411d0?person=961f6b00-713c-53f7-e053-b784100aa83b> owl:sameAs <https://archief.amsterdam/indexen/deeds/76803bae-cbec-4461-8749-620069ea686f?person=961f6b10-f7fe-53f7-e053-b784100aa83b> .
+        <https://archief.amsterdam/indexen/deeds/9d6d21e3-0133-666d-e053-b784100a1840?person=4d0a5597-9b56-7040-e053-b784100a6264> owl:sameAs <https://archief.amsterdam/indexen/deeds/a3cb51cc-b18e-50ee-e053-b784100a6caf?person=d4b2498e-8470-6faf-0510-0c935c356c0a> .
 
-        <https://data.goldenagents.org/.well-known/genid/Nc5f7e3e4703548608811eb88a231fd4d> a rdf:Statement ;
-            rdf:object <https://archief.amsterdam/indexen/deeds/76803bae-cbec-4461-8749-620069ea686f?person=961f6b10-f7fe-53f7-e053-b784100aa83b> ;
+        <https://data.goldenagents.org/.well-known/genid/N0000963728874509a598be4915758992> a rdf:Statement ;
+            rdf:object <https://archief.amsterdam/indexen/deeds/a3cb51cc-b18e-50ee-e053-b784100a6caf?person=d4b2498e-8470-6faf-0510-0c935c356c0a> ;
             rdf:predicate owl:sameAs ;
-            rdf:subject <https://archief.amsterdam/indexen/deeds/0cda93b1-ad78-4dc1-a835-42b1249411d0?person=961f6b00-713c-53f7-e053-b784100aa83b> ;
-            prov:wasDerivedFrom "ggd_otr_doop_clusters_t0.70_k10.tsv" ;
-            voidPlus:hasClusterID 1356 .
+            rdf:subject <https://archief.amsterdam/indexen/deeds/9d6d21e3-0133-666d-e053-b784100a1840?person=4d0a5597-9b56-7040-e053-b784100a6264> ;
+            prov:wasDerivedFrom "ggd_clusters_t0.70_k10_validation.tsv" ;
+            voidPlus:hasClusterID 3469 ;
+            voidPlus:hasValidation <https://data.goldenagents.org/.well-known/genid/Nb786c65b22bc413bb3a8629b5e79a9ba> .
+
+        <https://data.goldenagents.org/.well-known/genid/Nb786c65b22bc413bb3a8629b5e79a9ba> a voidPlus:Validation ;
+            voidPlus:hasValidationStatus <https://lenticularlens.org/voidPlus/resource/Accepted> .
         ```
 
     Args:
         infile (str): path to the csv/tsv file
         outfile (str): path to the output file (.trig)
+        reification (bool): wether or not to reify the link using an rdf:Statement (default: True)
 
     """
 
@@ -49,6 +57,13 @@ def main(infile, outfile):
     elif infile.endswith(".tsv"):
         df = pd.read_csv(infile, sep="\t")
 
+    # Check validation status
+    if "valid" in df.columns:
+        VALIDATION = True
+        df = df[df["valid"] == True]  # only keep valid rows
+    else:
+        VALIDATION = False
+
     # Group by cluster
     df_grouped = df.groupby("cluster_id")
 
@@ -58,21 +73,36 @@ def main(infile, outfile):
     for cluster_id, df_cluster in df_grouped:
         uris = df_cluster["uri"]
 
+        # Due to validation, there could be a cluster of one URI
+        if len(uris) == 1:
+            continue
+
         # Better explicit, so take combinations
         combs = combinations(uris, 2)
 
         for r1, r2 in combs:
             g.add((URIRef(r1), OWL.sameAs, URIRef(r2)))
 
-            # Bit of reification
-            statement = Resource(g, BNode())
-            statement.add(RDF.type, RDF.Statement)
-            statement.add(RDF.subject, URIRef(r1))
-            statement.add(RDF.predicate, OWL.sameAs)
-            statement.add(RDF.object, URIRef(r2))
+            if reification:
+                # Bit of reification
+                statement = Resource(g, BNode())
+                statement.add(RDF.type, RDF.Statement)
+                statement.add(RDF.subject, URIRef(r1))
+                statement.add(RDF.predicate, OWL.sameAs)
+                statement.add(RDF.object, URIRef(r2))
 
-            statement.add(PROV.wasDerivedFrom, Literal(infile))
-            statement.add(voidPlus.hasClusterID, Literal(cluster_id))
+                statement.add(PROV.wasDerivedFrom, Literal(infile))
+                statement.add(voidPlus.hasClusterID, Literal(cluster_id))
+
+                if VALIDATION:  # only for the valid links
+                    validationStatus = Resource(g, BNode())
+                    validationStatus.add(RDF.type, voidPlus.Validation)
+                    validationStatus.add(
+                        voidPlus.hasValidationStatus,
+                        URIRef("https://lenticularlens.org/voidPlus/resource/Accepted"),
+                    )
+
+                    statement.add(voidPlus.hasValidation, validationStatus)
 
     # Skolemize
     g = skolemize(g)
@@ -86,6 +116,9 @@ def main(infile, outfile):
 
 
 def skolemize(g):
+    """
+    Skolemize all blank node identifiers into .well-known URIs because we don't like blank nodes.
+    """
 
     new_g = Graph(identifier=g.identifier)
     g = g.skolemize(new_graph=new_g, authority=ga, basepath=skolem_genid)
